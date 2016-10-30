76c15c63a3d2e5f1e91794293518822337f104d3
Adam <apacz_0@o2.pl>
2016-10-30 18:22:28 +0100

    initial commit

---
+++ src/main/java/domain/gui/FXMLController.java
@@ -0,0 +1,17 @@
+package domain.gui;
+ 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.layout.HBox;
+import javafx.scene.text.Text;
+ 
+public class FXMLController {
+    @FXML 
+    private Text actiontarget;
+    
+    @FXML 
+    protected void handleSubmitButtonAction(ActionEvent event) {
+        actiontarget.setText("Sign in button pressed");
+    }
+
+}
