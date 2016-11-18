b9c9e0fb0a6bf7f61bc0a4a469ce590fa9e42c24
Adam <apacz_0@o2.pl>
2016-11-18 14:25:55 +0100

    mybatis added

--- backend/src/main/java/com/malinki/pz/Application.java
+++ backend/src/main/java/com/malinki/pz/Application.java
@@ -1,7 +1,25 @@
 package com.malinki.pz;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+
+import javax.sql.DataSource;
+
+import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.mapping.Environment;
+import org.apache.ibatis.session.Configuration;
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+import org.apache.ibatis.transaction.TransactionFactory;
+import org.apache.ibatis.transaction.jdbc.JdbcTransactionFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import com.malinki.pz.controller.Animal;
+import com.malinki.pz.controller.User;
 
 @SpringBootApplication
 public class Application {
@@ -9,6 +27,28 @@ public class Application {
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
 		System.out.println("Gosh, it's working!");
+
+		String resource = "mybatis-config.xml";
+		InputStream inputStream = null;
+		try {
+			inputStream = Resources.getResourceAsStream(resource);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder().build(inputStream);
+
+		/////
+		
+		SqlSession session = sqlSessionFactory.openSession();
+		try {
+			User user = session.selectOne("mapper.test");
+			User user2 = session.selectOne("mapper.test2", "12:0");              
+
+			System.out.println("lol1: " + user.getUserId());
+			System.out.println("lol2: " + user2.getUserId());
+		} finally {
+			session.close();
+		}
 	}
 
 }
---
+++ backend/src/main/java/com/malinki/pz/controller/User.java
@@ -0,0 +1,29 @@
+package com.malinki.pz.controller;
+
+public class User {
+    private String userId;
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String imageUri;
+
+    public String getUserId() {
+        return userId;
+    }
+    
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getImageUri() {
+        return imageUri;
+    }
+}
