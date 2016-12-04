b5c884a79b572ba8cf528c72462a0abd11c7ae85
niebezpieczny <niebezpieczny@o2.pl>
2016-12-04 11:04:15 +0100

    Abstract class for user repository created

---
+++ backend/src/main/java/com/malinki/pz/AbstractUserRepository.java
@@ -0,0 +1,101 @@
+package com.malinki.pz;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStreamWriter;
+
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+import org.apache.log4j.Level;
+import org.apache.log4j.Logger;
+
+import com.malinki.pz.controller.UserController;
+
+import constants.Strings;
+
+public abstract class AbstractUserRepository {
+	private Logger logger = Logger.getLogger(UserController.class);
+
+	public void performAction(HttpServletResponse response){
+		InputStream inputStream = openInputStream();
+		SqlSession session = establishSession(inputStream);
+		boolean isActionFinishedSuccesfully = false;
+		
+		Mapper mapper = session.getMapper(Mapper.class);	
+		
+		try {				
+			mainAction(mapper);
+			logger.log(Level.INFO, String.format(Strings.USER_ADDED_PROPERLY_INFO, mapper.getLastUser().getLogin()));
+			
+			isActionFinishedSuccesfully = true;
+		} catch(Exception e){
+			logger.log(Level.ERROR, e.toString());
+		} finally {
+			session.close();
+			closeInputStream(inputStream);
+		}
+		
+		setResponse(response, isActionFinishedSuccesfully);
+	}	
+	
+	private SqlSession establishSession(InputStream inputStream){
+		SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder().build(inputStream);	
+		return sqlSessionFactory.openSession();
+	}
+
+	private InputStream openInputStream(){
+		InputStream inputStream = null;
+		
+		try {
+			inputStream = Resources.getResourceAsStream(Application.MYBATIS_CONFIG_FILE_NAME);
+		} catch (IOException e) {
+			logger.log(Level.ERROR, e.toString());
+		}
+		
+		return inputStream;
+	}
+
+
+	private void setResponse(HttpServletResponse response, boolean isActionFinishedSuccesfully){
+		OutputStreamWriter writer = null;
+		
+		try{
+			if(isActionFinishedSuccesfully)
+				response.setStatus(HttpServletResponse.SC_OK);
+			else
+				response.setStatus(HttpServletResponse.SC_BAD_REQUEST);
+
+			writer = new OutputStreamWriter(response.getOutputStream());
+
+			if(isActionFinishedSuccesfully)
+				writer.write(Strings.USER_ADDED_PROPERLY);
+			else
+				writer.write(Strings.USER__NOT_ADDED_PROPERLY);
+
+			writer.flush();
+		} catch(IOException e){
+			logger.log(Level.ERROR, e.toString());
+		}
+		finally{
+			try {
+				writer.close();
+			} catch (IOException e) {
+				logger.log(Level.ERROR, e.toString());
+			}
+		}
+	}
+
+	private void closeInputStream(InputStream inputStream){
+		try {
+			inputStream.close();
+		} catch (IOException e) {
+			logger.log(Level.ERROR, e.toString());
+		}
+	}
+	
+	abstract public void mainAction(Mapper mapper);
+}
--- backend/src/main/java/com/malinki/pz/controller/UserController.java
+++ backend/src/main/java/com/malinki/pz/controller/UserController.java
@@ -8,21 +8,23 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.malinki.pz.ApplicationContextProvider;
-import com.malinki.pz.BeanNamesEnum;
 import com.malinki.pz.UserRepository;
+
+import constants.BeanNamesEnum;
+import constants.ParamNamesEnum;
 
 
 @RestController
 @RequestMapping(value="/api")            
-public class Controller {
+public class UserController {
 		
 	public UserRepository repository;
 	
 	@RequestMapping(value = "/login")
 	public void login(@RequestBody String requestBody, HttpServletResponse response) {		
 		JSONObject jsonObject = new JSONObject(requestBody);
-		String login = jsonObject.getString("login");		
-		String password = jsonObject.getString("password");	
+		String login = jsonObject.getString(ParamNamesEnum.LOGIN.getName());		
+		String password = jsonObject.getString(ParamNamesEnum.PASSWORD.getName());	
 
 		repository = ApplicationContextProvider.getApplicationContext().getBean(BeanNamesEnum.USER_REPOSITORY.getName(), UserRepository.class);
 		repository.registerUser(response, login, password);
