74a0e5c5a2b8b2c1d9265d014a7f597ed2d886f2
Adam <apacz_0@o2.pl>
2017-01-04 23:58:04 +0100

    Dynamic SQL used

--- dal/src/main/java/com/malinki/pz/dal/FlightMapper.java
+++ dal/src/main/java/com/malinki/pz/dal/FlightMapper.java
@@ -4,8 +4,10 @@ import com.malinki.pz.lib.FlightDTO;
 import com.malinki.pz.lib.FlightToSearchDTO;
 import com.malinki.pz.lib.TicketDTO;
 import com.malinki.pz.lib.UserDTO;
+import org.apache.ibatis.annotations.Lang;
 import org.apache.ibatis.annotations.Param;
 import org.apache.ibatis.annotations.Select;
+import org.apache.ibatis.scripting.xmltags.XMLLanguageDriver;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -23,15 +25,29 @@ public interface FlightMapper {
     @Select("SELECT DISTINCT src.Name FROM Flight, Airport src, Airport dest WHERE \"From\"=src.ID_Airport AND \"To\"=dest.ID_Airport AND dest.Name=#{dest}")
     ArrayList<String> getPossibleSourcesWithParam(@Param("dest") String dest);
 
-    @Select("SELECT Flight_Number AS flightNumber, departure_date AS departureDate, arrival_date AS arrivalDate, (base_price * Multiplier.Multiplier) AS price, Airline.name AS airlineName, " +
-            "src.Name AS \"From\", dest.Name AS \"To\", free_places AS freePlaces, Airline.name_Shortcut AS airlineShortcut " +
-            "FROM Flight, Airline, Airport src, Airport dest, Multiplier, \"CLASS\" " +
-            "WHERE Flight.ID_Airline = Airline.ID_Airline AND Flight.ID_Airline = Airline.ID_Airline " +
+    @Lang(XMLLanguageDriver.class)
+    @Select("<script>" +
+            "SELECT Flight_Number AS flightNumber, departure_date AS departureDate, arrival_date AS arrivalDate, (base_price * Multiplier.Multiplier) AS price, Airline.name AS airlineName, src.Name AS \"From\", dest.Name AS \"To\", free_places AS freePlaces, Airline.name_Shortcut AS airlineShortcut FROM Flight, Airline, Airport src, Airport dest, Multiplier, \"CLASS\" " +
+            "WHERE " +
+            "Flight.ID_Airline = Airline.ID_Airline AND Flight.ID_Airline = Airline.ID_Airline " +
             "AND src.ID_AIRPORT = \"From\" AND dest.ID_AIRPORT = \"To\" " +
-            "AND src.NAME = #{from} AND dest.NAME = #{to} " +
-            "AND Departure_Date BETWEEN #{dateStart} AND #{dateEnd} " +
-            "and Multiplier.ID_Multiplier = \"CLASS\".ID_Multiplier and \"CLASS\".Name = #{_class} " +
-            "AND free_places >= #{numberOfPassengers}")
+            "AND Multiplier.ID_Multiplier = \"CLASS\".ID_Multiplier" +
+            " <if test=\"from != null\">" +
+            "    AND src.NAME = #{from}" +
+            " </if>\n" +
+            " <if test=\"to != null\">" +
+            "    AND dest.NAME = #{to}" +
+            " </if>" +
+            "<if test=\"dateStart != null and dateEnd != null\">" +
+            "    AND Departure_Date BETWEEN #{dateStart} AND #{dateEnd} " +
+            " </if>" +
+            "<if test=\"_class != null\">" +
+            "   AND \"CLASS\".Name = #{_class}" +
+            "</if>" +
+            "<if test=\"numberOfPassengers != null\">" +
+            "   AND free_places >= #{numberOfPassengers}" +
+            "</if>" +
+            "</script>")
     ArrayList<FlightDTO> getFlights(FlightToSearchDTO flightToSearchDTO);
 
 
