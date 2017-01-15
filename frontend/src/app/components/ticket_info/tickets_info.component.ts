4b72d0e711476760a6eab587a8c49ebfa14eda3f
MoniRoz <monika.rozwadowska@outlook.com>
2017-01-15 17:07:25 +0100

    wyswietlenir liczby miejsc

---
+++ frontend/src/app/components/ticket_info/tickets_info.component.ts
@@ -0,0 +1,12 @@
+import {Component} from '@angular/core';
+
+@Component({
+  selector: 'ticket-info',
+  templateUrl: 'tickets_info.component.html',
+  styleUrls: ['tickets_info.component.css']
+})
+
+export class TicketInfo {
+  constructor() {
+  }
+}
