624747e0511e346068da5cc8f81ca10390200c86
MoniRoz <monika.rozwadowska@outlook.com>
2017-01-18 00:14:23 +0100

    walidacja maila

--- frontend/src/app/_validators/email-validator.ts
+++ frontend/src/app/_validators/email-validator.ts
@@ -1,7 +1,7 @@
 export class EmailValidator {
 
   static emailValidator(control) {
-    var EMAIL_REGEXP = /^[a-z0-9!#$%&'*+\/=?^_`{|}~.-]+@[a-z0-9]([a-z0-9-]*[a-z0-9])?(\.[a-z0-9]([a-z0-9-]*[a-z0-9])?)*$/i;
+    var EMAIL_REGEXP = /^[\w]+(?:\.[\w]+)*@(?:[a-zA-Z0-9-]+\.)+[a-zA-Z]{2,6}$/i;
 
     if (!EMAIL_REGEXP.test(control.value)) {
       return {invalidEmail: true};
