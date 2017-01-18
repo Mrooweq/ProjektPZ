04ce81c3987c0949680ed957f245bc5ed39df1dd
Adam <apacz_0@o2.pl>
2017-01-18 12:52:28 +0100

    Tabular function added

--- dal/src/main/java/com/malinki/pz/dal/mappers/TicketMapper.java
+++ dal/src/main/java/com/malinki/pz/dal/mappers/TicketMapper.java
@@ -31,18 +31,6 @@ public interface TicketMapper extends Mapper {
             " and Username = #{username}")
     List <TicketResponseDTO> getArchivalTickets(String username);
 
-    @Select("    select ID_Ticket as id, Airline.Name as airline, Name_Shortcut as airlineShortcut, Flight_number as flightNumber,\n" +
-            "    src.Name as \"from\", dest.Name as \"to\",\n" +
-            "    departure_date as departureDate, arrival_date as arrivalDate,\n" +
-            "    (base_price * multiplier * Number_Of_Places) as price, Number_Of_Places as numberOfPlaces, Class.NAME as flightClass, firstname, lastname, email\n" +
-            "    from Ticket, Flight, Class, \"User\", Airline, Multiplier, Airport src, Airport dest\n" +
-            "    where Ticket.ID_FLIGHT = Flight.ID_FLIGHT\n" +
-            "    and Ticket.ID_CLASS = Class.ID_CLASS\n" +
-            "    and Ticket.ID_USER = \"User\".ID_USER\n" +
-            "    and Flight.ID_AIRLINE = Airline.ID_AIRLINE\n" +
-            "    and Multiplier.ID_MULTIPLIER = Class.ID_MULTIPLIER\n" +
-            "    and Flight.\"From\" = src.ID_AIRPORT\n" +
-            "    and Flight.\"To\" = dest.ID_AIRPORT\n" +
-            "    and ID_Ticket = #{id}")
+    @Select("select * from table(pkg_utl.getTicket(#{id}))")
     TicketResponseDTO getTicketByID(int id);
 }
